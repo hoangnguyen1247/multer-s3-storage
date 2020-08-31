ae503cfa9b7e4416543545a129c78585d0b87298
Hoàng Ng <hoangnguyen1247@gmail.com>
2020-08-31 13:48:40 +0700

    initial: create project

---
+++ test/ExampleTest.test.ts
@@ -0,0 +1,13 @@
+import { describe, it } from "mocha";
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+import MulterS3Storage from "src/MulterS3Storage";
+
+chai.use(chaiAsPromised);
+
+describe("Test", () => {
+    describe("Test 1", () => {
+
+    })
+})
